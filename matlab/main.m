0c4cc9bed6a84d44cede3a0de5312b752f1f3dfe
Quim Llimona <ql@lemonzi.me>
2014-11-05 20:50:09 +0100

    batch processing

--- matlab/main.m
+++ matlab/main.m
@@ -1,20 +1,31 @@
 clear all;
 close all;
-
-filename = '../../Workshop - Shared/data/Dog_1_interictal_segment_0001.mat';
-settings = loadjson('./settings.json');
-bands = processSample(filename, settings);
-
-[nch, nbands, nwin] = size(bands);
-
 figure;
+
+folder = '~/Projects/UAB-Seizure Prediction/Dog_1/';
+files = dirPattern([folder '*preictal*.mat']);
+settings = loadjson('./settings.json');
 mkdir('out');
-for i = 1:nwin
-    imagesc(bands(:,:,i));
-    axis off;
-    axis square;
-    export_fig(sprintf('out/img_%03d',i), '-transparent');
-    clf;
+mkdir('videos');
+setenv('PATH', [getenv('PATH'), ':/usr/local/bin']);
+
+for i = 1:numel(files)
+
+    % filename = '../../Workshop - Shared/data/Dog_1_interictal_segment_0001.mat';
+    filename = files{i};
+    bands = processSample([folder filename], settings);
+    [nch, nbands, nwin] = size(bands);
+
+    mkdir(['out/', filename(1:end-4)]);
+    for j = 1:nwin
+        imagesc(bands(:,:,j)');
+        axis off;
+        axis equal;
+        export_fig(['out/', filename(1:end-4), sprintf('/img_%03d.png',j)], '-transparent');
+        clf;
+    end
+    setenv('OUTPUT_VIDEO_FN', ['videos/', filename(1:end-4), '.mp4'])
+    setenv('INPUT_FOLDER', ['out/', filename(1:end-4)])
+    !ffmpeg -framerate 4 -i $INPUT_FOLDER/img_%03d.png -c:v libx264 -r 30 $OUTPUT_VIDEO_FN &
+
 end
-!ffmpeg -framerate 3 -i out/img_%03d.png -c:v libx264 -r 30  out.mp4
-!rm out/*.png
